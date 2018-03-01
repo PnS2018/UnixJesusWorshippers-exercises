18ee8333a1f5ba65e28fbe9b365f3c564e9320d9
Michel Perez <michel.perez@gmx.ch>
2018-03-01 17:06:29 +0100

    added soltuion to ex3

--- session_1.py
+++ session_1.py
@@ -8,15 +8,19 @@ a=K.placeholder(shape=(5,))
 b=K.placeholder(shape=(5,))
 c=K.placeholder(shape=(5,))
 
-funcOut= a**2 + b**2 + c**2 + 2*b*c
+funcOut1= a**2 + b**2 + c**2 + 2*b*c
 
-sumx= K.function(inputs=(a, b, c), outputs=(funcOut,))
+sumx= K.function(inputs=(a, b, c), outputs=(funcOut1,))
 
 # EX.2
 
 # EX.3
 
+w=K.placeholder(shape=(2,))
+b=K.placeholder(shape=(1,))
+x=K.placeholder(shape=(2,))
 
-
+funcOut2=1/(1+(1/K.exp(w[0]*x[0]+w[1]*x[1]+b)))
+multElements= K.function(inputs=(w, b, x), outputs=(funcOut2,))
 
 # EX.4
