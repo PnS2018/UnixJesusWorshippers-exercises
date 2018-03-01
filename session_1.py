a5464ec190f030dc9dcd0283c9fa588788f33e4c
Michel Perez <michel.perez@gmx.ch>
2018-03-01 16:52:42 +0100

    added solution to ex1

--- session_1.py
+++ session_1.py
@@ -1,9 +1,22 @@
 # SESSION 1, EXERCISES
+import numpy as np
+from keras import backend as K
 
 # EX.1
+
+a=K.placeholder(shape=(5,))
+b=K.placeholder(shape=(5,))
+c=K.placeholder(shape=(5,))
+
+funcOut= a**2 + b**2 + c**2 + 2*b*c
+
+sumx= K.function(inputs=(a, b, c), outputs=(funcOut,))
 
 # EX.2
 
 # EX.3
+
+
+
 
 # EX.4
