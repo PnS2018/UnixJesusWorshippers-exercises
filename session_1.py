5bf8353c6e30cb7bfc31cfde6751d3db9adfdd5a
Eric Marty <mrdederi@gmail.com>
2018-03-01 17:08:35 +0100

    Added exercise 2

--- session_1.py
+++ session_1.py
@@ -1,1 +1,14 @@
-print('Hello')
+# Exercise 2
+
+from keras import backend as K
+
+x = K.ones(shape=())
+
+tanh_tensor = (K.exp(x) - K.exp(-x)) / (K.exp(x) + K.exp(-x))
+
+grad_tanh_tensor = K.gradients(loss=tanh_tensor, variables=[x])
+
+tanh_functions = K.function(inputs=[x], outputs=[tanh_tensor, grad_tanh_tensor[0]])
+
+for i in [-100, -1, 0, 1, 100]:
+    print(tanh_functions([i]))
