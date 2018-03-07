4f8e2acb60711ddca730410fbf7435f8cfcce9bd
speedyduke <0xgstoffel@gmail.com>
2018-03-07 20:32:16 +0100

    ex 4 but not really working

--- session_1.py
+++ session_1.py
@@ -1,4 +1,5 @@
 # SESSION 1, EXERCISES
+import numpy as np
 from keras import backend as K
 
 # EX.1
@@ -34,3 +35,20 @@ funcOut2 = 1 / (1 + (1 / K.exp(w[0] * x[0] + w[1] * x[1] + b)))
 multElements = K.function(inputs=(w, b, x), outputs=(funcOut2,))
 
 # EX.4
+
+n = 4 # arbitrary number
+x = np.ones(n)
+coeff = K.placeholder(shape=(n, 1))
+n_vec = np.arange(n)
+p = K.placeholder(shape=())
+
+monome_vec = np.power(x,n_vec)
+
+if coeff.shape == monome_vec.shape:
+    p = coeff * x
+
+grad_polynome = K.gradients(loss=p, variables=(monome_vec,))
+grad_function = K.function(inputs=(coeff), outputs=(grad_polynome)) #why doesnt this work?
+
+
+print(grad_function(np.array([1,3,5,7])))
