d0047b4aaa80d005fb3899daadd51beb8e596c4e
Eric Marty <mrdederi@gmail.com>
2018-03-08 16:16:35 +0100

    Keras Exercise solutions from Eric

--- session_02/logistic-regression-with-keras-layers-template.py
+++ session_02/logistic-regression-with-keras-layers-template.py
@@ -1,13 +1,13 @@
 """Logistic Regression for Fashion MNIST Binary Classification.
 
-Team #name
+Team UnixJesusWorshippers
 """
 from __future__ import print_function, absolute_import
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Activation
 from keras.models import Model
 
 from pnslib import utils
@@ -17,21 +17,21 @@ from pnslib import ml
 # complete label description is at
 # https://github.com/zalandoresearch/fashion-mnist#labels
 # >>>>> Try to change the class list for other classes <<<<<
+
 (train_x, train_y, test_x, test_y) = utils.binary_fashion_mnist_load(
     class_list=[0, 1],
     flatten=True)
 
-print ("[MESSAGE] Dataset is loaded.")
-
+print("[MESSAGE] Dataset is loaded.")
 
 # preprocessing for training and testing images
-train_x = train_x.astype("float32")/255.  # rescale image
+train_x = train_x.astype("float32") / 255.  # rescale image
 mean_train_x = np.mean(train_x, axis=0)  # compute the mean across pixels
 train_x -= mean_train_x  # remove the mean pixel value from image
-test_x = test_x.astype("float32")/255.
+test_x = test_x.astype("float32") / 255.
 test_x -= mean_train_x
 
-print ("[MESSAGE] Dataset is preporcessed.")
+print("[MESSAGE] Dataset is preporcessed.")
 
 # Use PCA to reduce the dimension of the dataset,
 # so that the training will be less expensive
@@ -41,14 +41,13 @@ train_X, R, n_retained = ml.pca(train_x)
 # perform PCA on testing dataset
 test_X = ml.pca_fit(test_x, R, n_retained)
 
-print ("[MESSAGE] PCA is complete")
+print("[MESSAGE] PCA is complete")
 
 # define a model
-# >>>>> PUT YOUR CODE HERE <<<<<
-
-
-
-
+x = Input((train_X.shape[1],), name="input_layer")
+y = Dense(1, name="linear_layer")(x)
+y = Activation("sigmoid")(y)
+model = Model(x, y)
 
 # print model summary
 model.summary()
@@ -57,22 +56,18 @@ model.summary()
 # and use SGD optimizer, you can try to use different
 # optimizers if you want
 # see https://keras.io/losses/
-# >>>>> PUT YOUR CODE HERE <<<<<
+model.compile(loss="mean_squared_error",
+              optimizer="sgd",
+              metrics=["mse"])
 
-
-
-
-
-
-print ("[MESSAGE] Model is compiled")
+print("[MESSAGE] Model is compiled")
 
 # train the model with fit function
 # See https://keras.io/models/model/ for usage
-# >>>>> PUT YOUR CODE HERE <<<<<
-
-
-
-
+model.fit(
+    x=train_X, y=train_y,
+    batch_size=64, epochs=10,
+    validation_data=(test_X, test_y))
 
 # save the trained model
 model.save("logistic-regression-fashion-mnist-trained.hdf5")
@@ -89,9 +84,9 @@ labels = ["Tshirt/top", "Trouser"]
 plt.figure()
 for i in xrange(2):
     for j in xrange(5):
-        plt.subplot(2, 5, i*5+j+1)
-        plt.imshow(test_x[i*5+j].reshape(28, 28), cmap="gray")
+        plt.subplot(2, 5, i * 5 + j + 1)
+        plt.imshow(test_x[i * 5 + j].reshape(28, 28), cmap="gray")
         plt.title("Ground Truth: %s, \n Prediction %s" %
-                  (labels[ground_truths[i*5+j]],
-                   labels[preds[i*5+j]]))
+                  (labels[ground_truths[i * 5 + j]],
+                   labels[preds[i * 5 + j]]))
 plt.show()
